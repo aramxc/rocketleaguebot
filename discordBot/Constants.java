af8c7fb8a0d0989b9704fd10e6e1c2a096d00829
Aaron <30804770+aramxc@users.noreply.github.com>
2017-09-23 08:40:34 -0600

    Update Constants.java

--- discordBot/Constants.java
+++ discordBot/Constants.java
@@ -7,8 +7,8 @@ package discordBot;
 
 public class Constants {
 	// discord Token via Discord for Developers url: "https://discordapp.com/developers/"
-	public static final String discordToken = "MzU1NDM3NzI0OTQ3NDQ3ODA5.DJiPdw.ZcZE_eAwEqtU7Vkka_5pHr5OcNg";
+	public static final String discordToken = "[DISCORD_TOKEN_HERE]";
 	// rlsKey via RLS Developers url: "https://developers.rocketleaguestats.com/"
-	public static final String rlsKey = "N6Z5FWBCLC7PNIG13Y0L90TEC0OHN933";
+	public static final String rlsKey = "[RLSTATS_TOKEN_HERE]";
 	
 }
